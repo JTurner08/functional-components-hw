82e3a4166a6145050ca498bc8bf6fc10a479dc07
Jarquise Turner <j.turner348@gmail.com>
2021-09-09 03:13:59 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import FloorPlan from "./components/FloorPlan"
+//import Kitchen from "./components/Kitchen"
+//import LivingRoom from "./components/LivingRoom"
+//import Bedroom from "./components/Bedroom"
+//import Bath from "./components/Bath"
+// import Oven from "./components/Oven"
+// import Sink from "./components/Sink"
+import "./App.css"
+
+
+function App() {
+
+  return (
+    <div className="container">
+      <FloorPlan/>
+    </div>
+  )
+}
+
+export default App
