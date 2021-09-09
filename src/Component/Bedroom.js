e45f432e3a0f037f87100bcef41056680f41c035
Jarquise Turner <j.turner348@gmail.com>
2021-09-09 03:14:30 -0400

    Create Bedroom.js

---
+++ src/Component/Bedroom.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function Bedroom(props) {
+   console.log(props)
+    return (
+        <div className="bedroom" id={props.bedroom}>
+           <h3>{props.bedroom}</h3> 
+        </div> 
+        
+    )
+}
+
+export default Bedroom
