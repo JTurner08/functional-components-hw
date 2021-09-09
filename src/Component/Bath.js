08d99c0e9e4a907ddf3e35766362ea126dcf28f9
Jarquise Turner <j.turner348@gmail.com>
2021-09-09 03:14:12 -0400

    Create Bath.js

---
+++ src/Component/Bath.js
@@ -0,0 +1,13 @@
+  
+import React from 'react'
+
+function Bath(props) {
+    return (
+     <div className="bathroom" id={props.bathroom}>
+       <h3> {props.bathroom}</h3>
+    </div>
+        
+    )
+}
+
+export default Bath
